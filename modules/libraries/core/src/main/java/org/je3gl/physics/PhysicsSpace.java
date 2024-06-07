032c8dda05ff118993355c5b6953b39be773bec2
wil <wil@debian>
2024-06-07 17:14:52 -0600

    update

--- modules/libraries/core/src/main/java/org/je3gl/physics/PhysicsSpace.java
+++ modules/libraries/core/src/main/java/org/je3gl/physics/PhysicsSpace.java
@@ -31,6 +31,9 @@
  */
 package org.je3gl.physics;
 
+import com.jme3.scene.Spatial;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.je3gl.physics.control.PhysicsBody2D;
 import org.je3gl.physics.control.PhysicsControl;
 import org.je3gl.physics.joint.PhysicsJoint;
@@ -51,7 +54,9 @@ import org.dyn4j.world.World;
  * @since 1.0.0
  */
 public class PhysicsSpace<E extends PhysicsBody2D> extends World<E> {
-    
+
+    /** Class logger. */
+    private static final Logger LOGGER = Logger.getLogger(PhysicsSpace.class.getName());    
     /** physics default speed. */
     private static final float DEFAULT_SPEED = 1.0F;
     
@@ -75,14 +80,66 @@ public class PhysicsSpace<E extends PhysicsBody2D> extends World<E> {
             this.setBounds(bounds);
         }
     }
+    
+    /**
+     * Adds a new physical object to the world (physical space)
+     * @param body the body
+     */
+    public void addPhysicsBody(Object body) {
+        this.addPhysicsBody(body, false);
+    }
 
+    /**
+     * Adds a new physical object to the world (physical space)
+     * @param body the body
+     * @param notifyFirst <code>true</code> if the body is added to the physical
+     * space first and this physical space is notified (set) to the control 
+     * later, and <code>false</code> if the opposite is true.
+     */
+    @SuppressWarnings("unchecked")
+    public void addPhysicsBody(Object body, boolean notifyFirst) {
+        PhysicsBody2D body2D = null;
+        if (body instanceof Spatial) {
+            body2D = ((Spatial) body).getControl(PhysicsBody2D.class);
+        } else if (body instanceof PhysicsBody2D) {
+            body2D = (PhysicsBody2D) body;
+        }
+        
+        if (body2D == null) {
+            LOGGER.log(Level.WARNING, "Object [{0}] is not a physical body supported by Dyn4j", body);
+        } else {
+            this.addBody((E) body2D, notifyFirst);
+        }
+    }
+    
+    /**
+     * Remove a physical body from this space.
+     * @param body the body
+     * @return boolean
+     */
+    @SuppressWarnings("unchecked")
+    public boolean removePhysicsBody(Object body) {
+        PhysicsBody2D body2D = null;
+        if (body instanceof Spatial) {
+            body2D = ((Spatial) body).getControl(PhysicsBody2D.class);
+        } else if (body instanceof PhysicsBody2D) {
+            body2D = (PhysicsBody2D) body;
+        }
+        
+        if (body2D == null) {
+            LOGGER.log(Level.WARNING, "Object [{0}] is not a physical body supported by Dyn4j", body);
+            return false;
+        } else {
+            return this.removeBody((E) body2D);
+        }
+    }
+    
     /**
      * (non-Javadoc)
      * @see org.dyn4j.world.AbstractCollisionWorld#addBody(org.dyn4j.collision.CollisionBody)
      * @param body body
      */
     @Override
-    @SuppressWarnings("unchecked")
     public void addBody(E body) {
         addBody(body, false);
     }
@@ -136,8 +193,20 @@ public class PhysicsSpace<E extends PhysicsBody2D> extends World<E> {
      * @see jme3gl2.physics.PhysicsSpace#addJoint(org.dyn4j.dynamics.joint.Joint) 
      * @param joint joint
      */
-    public void addJoint(final PhysicsJoint<E, ? extends Joint<E>> joint) {
-        this.addJoint(joint.getJoint());
+    @SuppressWarnings("unchecked")
+    public void addPhysicsJoint(Object joint) {
+        Joint<E> myJoint = null;
+        if (joint instanceof Joint) {
+            myJoint = (Joint<E>) joint;
+        } else if (joint instanceof PhysicsJoint) {
+            myJoint = ((PhysicsJoint<E, ?>) joint).getJoint();
+        }
+        
+        if (myJoint == null) {
+            LOGGER.log(Level.WARNING, "Unsupported joint: {0}", joint);
+        } else {
+            this.addJoint(myJoint);
+        }
     }
     
     /**
@@ -146,8 +215,21 @@ public class PhysicsSpace<E extends PhysicsBody2D> extends World<E> {
      * @param joint joint
      * @return boolean
      */
-    public boolean removeJoint(final PhysicsJoint<E, ? extends Joint<E>> joint) {
-        return this.removeJoint(joint.getJoint());
+    @SuppressWarnings("unchecked")
+    public boolean removePhysicsJoint(Object joint) {
+        Joint<E> myJoint = null;
+        if (joint instanceof Joint) {
+            myJoint = (Joint<E>) joint;
+        } else if (joint instanceof PhysicsJoint) {
+            myJoint = ((PhysicsJoint<E, ?>) joint).getJoint();
+        }
+        
+        if (myJoint == null) {
+            LOGGER.log(Level.WARNING, "Unsupported joint: {0}", joint);
+            return false;
+        } else {
+            return this.removeJoint(myJoint);
+        }
     }
     
     /**
